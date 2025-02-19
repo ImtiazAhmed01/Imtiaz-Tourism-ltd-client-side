7fe15a1481e1477533d34c38dbe8348cc0fcd736
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-02-19 08:43:06 -0500

    chart and dark mode fixing

--- src/Component/Overstate/Overstate.jsx
+++ src/Component/Overstate/Overstate.jsx
@@ -14,7 +14,7 @@ const Overstate = () => {
     });
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/stats')  // Replace with your backend API URL
+        fetch('https://imtiaztourismltdd.vercel.app/api/stats')  // Replace with your backend API URL
             .then((response) => response.json())
             .then((data) => {
                 setStats(data);
